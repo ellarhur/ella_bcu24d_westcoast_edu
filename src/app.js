e5faa0317fde871673e49db2f4994e78b96706b8
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-01-27 17:15:34 +0100

    [Add] slide function to landing page in app.js

--- src/app.js
+++ src/app.js
@@ -20,3 +20,39 @@ const init = async () => {
 };
 
 init()
+
+
+// LANDING PAGE SLIDESHOW HÄMTAD FRÅN W3 SCHOOLS
+import {} from '';
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
