765c54a1d02dae09660f5fbaaf92b3b70a3e415e
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-02-08 15:17:36 +0100

    [Add] courses as cards on courses.html!

--- dist/models/courses.js
+++ dist/models/courses.js
@@ -17,19 +17,35 @@ const displayCourses = (courses) => {
         const div = document.createElement('div');
         const imageAnchor = document.createElement('a');
         const image = document.createElement('img');
-        const cardBody = document.createElement('div');
-        const heading = document.createElement('h5');
+        const coursecardBody = document.createElement('div');
+        const heading = document.createElement('h3');
         const p = document.createElement('p');
         const small = document.createElement('small');
-        div.classList.add("card");
+        div.classList.add("coursecard");
         imageAnchor.href = `.pages/course-details.html?id=${course.id}`;
         image.alt = `${course.title}`;
         image.src = course.image
             ? `/src/assets${course.image}`
-            : '/src/assets/default.png';
-        // imageAnchor.appendChild(image);
-        // div.appendChild(imageAnchor);
-        // app.appendChild(div);
+            : `/src/assets/default.jpg`;
+        imageAnchor.appendChild(image);
+        div.appendChild(imageAnchor);
+        coursecardBody.classList.add('coursecard-body');
+        heading.classList.add('coursecard-title');
+        heading.textContent = course.title;
+        const dateText = document.createElement('p');
+        dateText.textContent = `Datum för kursen: ${course.dates}`;
+        const classroomText = document.createElement('p');
+        classroomText.textContent = `${course.classroom ? "Kursen hålls på plats" : "Kursen är endast online"}`;
+        const priceText = document.createElement('p');
+        priceText.textContent = `Kostnad: ${course.price}`;
+        small.appendChild(dateText);
+        small.appendChild(classroomText);
+        small.appendChild(priceText);
+        p.appendChild(small);
+        coursecardBody.appendChild(heading);
+        coursecardBody.appendChild(p);
+        div.appendChild(coursecardBody);
+        app.appendChild(div);
     }
 };
 document.addEventListener('DOMContentLoaded', initApp);
