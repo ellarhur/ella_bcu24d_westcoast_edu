c64190f85cd5261fb6e214823cea6a60d3162c7d
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-02-12 17:58:27 +0100

    [Add] the final changes to the login and create account javascript code

--- dist/models/login.js
+++ dist/models/login.js
@@ -1,1 +1,15 @@
-"use strict";
+const form = document.querySelector(".registerForm");
+
+const initApp = () => {};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+    const username = data.get('username');
+    const password = data.get('password');
+
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
+form.addEventListener('submit', handleSubmit);
